5229e7dab84bd5827ec2e092af4372cfa2103e5d
kaityhallman <kaitlinhallman@gmail.com>
2016-02-05 19:17:04 -0500

    updated views, data, cleaned up code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,59 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+    var markers = [
+    {
+        "title": 'Uptown',
+        "lat": '35.230681',
+        "lng": '-80.839608', 
+        "description": '<h3 class="marker"><strong>Uptown</strong></h3><p class="marker">401 N. Tryon St., Suite 100</p>'
+    },
+    {
+        "title": 'South End',
+        "lat": '35.206857',
+        "lng": '-80.860960',
+        "description": '<h3 class="marker"><strong>South End</strong></h3><p class="marker">2201 South Blvd</p>'
+    },
+    {
+        "title": 'North Davidson',
+        "lat": '35.240637',
+        "lng": '-80.814849',
+        "description": '<h3 class="marker"><strong>NoDa</strong></h3><p class="marker">2229 N Davidson Street</p>'
+    }
+    ];
+    window.onload = function () {
+        LoadMap();
+    }
+    function LoadMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+ 
+        //Create and open InfoWindow.
+        var infoWindow = new google.maps.InfoWindow();
+ 
+        for (var i = 0; i < markers.length; i++) {
+            var data = markers[i];
+            var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: data.title,
+                animation: google.maps.Animation.DROP
+            });
+ 
+            //Attach click event to the marker.
+            (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function (e) {
+                    //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                    infoWindow.open(map, marker);
+                });
+            })(marker, data);
+        }
+    }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028163038) do
+ActiveRecord::Schema.define(version: 20160205225015) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "street"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151028163038) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
